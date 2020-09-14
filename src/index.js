12be74a22fc85be3d9d74cea256cce35eac302f2
Sander <peeters94@hotmail.com>
2020-09-14 15:01:58 +0200

    Added useDebouncedCallback hook, updated packages and added transpiling

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import useLatest from './useLatest'
+import useDebounce from './useDebounce'
+import useAsyncEffect from './useAsyncEffect'
+import useDebouncedCallback from './useDebouncedCallback'
+
+export {
+    useLatest,
+    useDebounce,
+    useAsyncEffect,
+    useDebouncedCallback,
+}
---
+++ src/useDebouncedCallback.js
@@ -0,0 +1,17 @@
+import { useRef, useCallback, useEffect } from 'react'
+
+export default function useDebouncedCallback(callback, delay, deps) {
+    const functionTimeoutHandler = useRef(null)
+    const debouncedFunction = useCallback(callback, deps)
+
+    useEffect(() => () => {
+        clearTimeout(functionTimeoutHandler.current)
+    }, [])
+
+    return (...args) => {
+        clearTimeout(functionTimeoutHandler.current)
+        functionTimeoutHandler.current = setTimeout(() => {
+            debouncedFunction(...args)
+        }, delay)
+    }
+}
