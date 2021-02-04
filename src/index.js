cfc7c55f0f98dde0c600c9dfb415e4b2ee4e55d4
Sander <peeters94@hotmail.com>
2021-02-04 16:08:40 +0100

    Added useOnClickOutside hook

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import useLatest from './useLatest'
 import useDebounce from './useDebounce'
 import useAsyncEffect from './useAsyncEffect'
+import useOnClickOutside from './useOnClickOutside'
 import useDebouncedCallback from './useDebouncedCallback'
 
 export {
     useLatest,
     useDebounce,
     useAsyncEffect,
+    useOnClickOutside,
     useDebouncedCallback,
 }
---
+++ src/useOnClickOutside.js
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+import useLatest from './useLatest'
+
+const MOUSEDOWN = 'mousedown'
+const TOUCHSTART = 'touchstart'
+const EVENTS = [MOUSEDOWN, TOUCHSTART]
+
+export default function useOnClickOutside(ref, handler) {
+    const handlerRef = useLatest(handler)
+    useEffect(() => {
+        if (!handler) {
+            throw new Error('No handler defined')
+        }
+        const listener = (event) => {
+            if (
+                !ref.current
+                || !handlerRef.current
+                || ref.current.contains(event.target)
+            ) {
+                return
+            }
+            handlerRef.current(event)
+        }
+        EVENTS.forEach((event) => {
+            document.addEventListener(event, listener)
+        })
+        return () => {
+            EVENTS.forEach((event) => {
+                document.removeEventListener(event, listener)
+            })
+        }
+    }, [handler, handlerRef, ref])
+}
